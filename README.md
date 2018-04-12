a4268040873530f726ea408d72d06940077c5b12
姚可鑫 <@xx.com>
2018-04-12 16:18:07 +0800

    antd 的基本使用

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DouBanMovie-React
+说明:
+- 1 ant-design 组件库
+- 2 react-router-dom 路由
+- 3 fetch API，配合Promise规范
+- 4 跨域获取数据
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+//导入 antd 组件
+import {DatePicker} from  'antd'
+
+//导入样式文件
+import 'antd/dist/antd.css'
+
+ReactDOM.render(
+    <DatePicker />,
+    document.getElementById('app')
+)
